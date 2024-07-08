42a271399d0115609f75ac23678c6a7edfabe2a7
sourabh punase <sourabhpunase19@gmail.com>
2024-07-08 15:17:28 +0530

    adding docker server

---
+++ index.js
@@ -0,0 +1,8 @@
+const express= require('express');
+const app= express();
+
+app.get('/',(req,res)=>{
+res.json({message:'this is docker file'})
+
+})
+app.listen(3300,()=>console.log('server running on 3300'))
